0c1b2dc7ccc8fd348ee84a5da34202fe2aa2cf0e
Mythic <mythicmaniac@gmail.com>
2019-04-28 05:13:55 +0300

    Refactor thunderstore API

--- manager/api/thunderstore.py
+++ manager/api/thunderstore.py
@@ -5,17 +5,31 @@ from .containers import Packages
 
 
 class ThunderstoreAPI:
-    def __init__(self, api_url):
-        self.api_url = api_url
+    def __init__(self, thunderstore_url):
+        self.thunderstore_url = thunderstore_url
+        self.api_url = parse.urljoin(thunderstore_url, "api", "v1")
+        self.package_url = parse.urljoin(self.api_url, "package")
         self.packages = Packages()
 
     def update_packages(self):
-        url = parse.urljoin(self.api_url, "package/")
-        data = requests.get(url).json()
+        data = requests.get(self.package_url).json()
         self.update_packages_with_data(data)
 
     def update_packages_with_data(self, data):
         self.packages.update(data)
 
+    # TODO: Deprecate
     def get_package_names(self):
         return [str(x) for x in self.packages.keys()]
+
+    def get_package_download_url(self, reference):
+        if not reference.version:
+            raise AttributeError("Unable to get package download URL without a version")
+        return parse.urljoin(
+            self.thunderstore_url,
+            "package",
+            "download",
+            reference.namespace,
+            reference.name,
+            reference.version,
+        )
